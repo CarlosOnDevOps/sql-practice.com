1c5af2b65f3e8760062ce81a68e9883a23aca0a3
Carlos <cmor3@protonmail.com>
2025-03-26 20:36:23 -0500

    added the folders and first few questions

---
+++ hospital-db/easy_solutions.sql
@@ -0,0 +1,63 @@
+/* 1. Show first name, last name, and gender of patients whose gender is 'M' */
+SELECT
+    first_name
+    , last_name
+FROM patients 
+WHERE
+    gender = 'M'
+
+
+/* 2. Show first name and last name of patients who does not have allergies. (null) */
+SELECT
+    first_name
+    , last_name
+FROM patients
+WHERE
+    allergies IS NULL;
+
+
+/* 3. Show first name of patients that start with the letter 'C' */ 
+SELECT
+    first_name
+FROM patients
+WHERE
+    first_name LIKE 'C%'
+
+
+/* 4. Show first name and last name of patients that weight within the range of 100 to 120 (inclusive) */ 
+    -- first let's create a subquery    
+
+/* 5. Show first name of patients that start with the letter 'C' */ 
+
+
+/* 6. Show first name of patients that start with the letter 'C' */ 
+
+
+/* 7. Show first name of patients that start with the letter 'C' */ 
+
+
+/* 8. Show first name of patients that start with the letter 'C' */ 
+
+
+/* 9. Show first name of patients that start with the letter 'C' */ 
+
+
+/* 10. Show first name of patients that start with the letter 'C' */ 
+
+
+/* 11. Show first name of patients that start with the letter 'C' */
+
+
+/* 12. Show first name of patients that start with the letter 'C' */ 
+
+
+/* 13. Show first name of patients that start with the letter 'C' */ 
+
+
+/* 14. Show first name of patients that start with the letter 'C' */ 
+
+
+/* 15. Show first name of patients that start with the letter 'C' */ 
+
+
+/* 16. Show first name of patients that start with the letter 'C' */ 
